1acc47c7ff75b4a854631ce4cae095f676bd7be9
Edward Farragher <edward.farragher@outlook.com>
2024-08-28 13:01:39 +0100

    Removing title hint

--- Team_Medals.py
+++ Team_Medals.py
@@ -57,9 +57,7 @@ st.markdown(
 def main():
     try:
         st.title(
-            "FPL Manager Medals: Team",
-            help="Note: The sidebar is hidden on mobile devices. Tap the > icon at the top left to access it. This includes getting medals for leagues and further information.",
-        )
+            "FPL Manager Medals: Team")
         if st.button(":twisted_rightwards_arrows: Switch to League Medals"):
             st.switch_page("pages/1_League_Medals.py")
 
